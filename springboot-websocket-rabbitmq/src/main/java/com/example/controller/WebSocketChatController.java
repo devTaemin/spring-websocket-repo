091ce7f576453612e32d74c19d32f8213a632e38
Taemin Im <devTaemin@gmail.com>
2023-04-25 15:00:02 +0900

    Update WebSocketChatController.java

--- springboot-websocket-rabbitmq/src/main/java/com/example/controller/WebSocketChatController.java
+++ springboot-websocket-rabbitmq/src/main/java/com/example/controller/WebSocketChatController.java
@@ -1,4 +1,27 @@
 package com.example.controller;
 
+
+// MessageController를 정의하는 2가지 방법
+// (1) SimpMessageSendingOperations을 사용하여 경로를 지정해주기
+// (2) @MessageMapping(), @SendTo, @Payload를 이용하기
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import com.example.domain.WebSocketChatMessage;
+@Controller
 public class WebSocketChatController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/javainuse")
+    public WebSocketChatMessage sendMessage(@Payload WebSocketChatMessage webSocketChatMessage) {
+        return webSocketChatMessage;
+    }
+    @MessageMapping("/chat.newUser")
+    @SendTo("/topic/javainuse")
+    public WebSocketChatMessage newUser(@Payload WebSocketChatMessage webSocketChatMessage,
+                                        SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", webSocketChatMessage.getSender());
+        return webSocketChatMessage;
+    }
 }
