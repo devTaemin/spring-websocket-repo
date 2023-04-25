e201d8627c4818eb4fe6317b1489d66da639447d
Taemin Im <devTaemin@gmail.com>
2023-04-25 13:27:01 +0900

    Initialize springboot websocket rabbitmq

--- springboot-websocket-rabbitmq/src/main/java/com/example/application/WebSocketChatApplication.java
+++ springboot-websocket-rabbitmq/src/main/java/com/example/application/WebSocketChatApplication.java
@@ -1,13 +1,13 @@
-package com.example.springbootwebsocketrabbitmq;
+package com.example.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootWebsocketRabbitmqApplication {
+public class WebSocketChatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootWebsocketRabbitmqApplication.class, args);
+		SpringApplication.run(WebSocketChatApplication.class, args);
 	}
 
 }
