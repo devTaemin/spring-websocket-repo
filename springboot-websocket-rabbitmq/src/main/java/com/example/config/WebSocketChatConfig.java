cf3531b6f313704d2b4e7d88c798a6ade1e9478e
Taemin Im <devTaemin@gmail.com>
2023-04-25 14:59:57 +0900

    Update WebSocketChatConfig.java

--- springboot-websocket-rabbitmq/src/main/java/com/example/config/WebSocketChatConfig.java
+++ springboot-websocket-rabbitmq/src/main/java/com/example/config/WebSocketChatConfig.java
@@ -1,4 +1,23 @@
 package com.example.config;
 
-public class WebSocketChatConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketChatConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocketApp").withSockJS();
+    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableStompBrokerRelay("/topic")
+                .setRelayHost("localhost")
+                .setVirtualHost("/")
+                .setRelayPort(61613)
+                .setClientLogin("guest")
+                .setClientPasscode("guest");
+    }
 }
