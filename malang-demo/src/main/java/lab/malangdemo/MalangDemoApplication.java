afcebfe6941ed6dae87377c74c895fd4f37223f4
Taemin Im <devTaemin@gmail.com>
2023-04-26 14:37:01 +0900

    Initialize malang demo

---
+++ malang-demo/src/main/java/lab/malangdemo/MalangDemoApplication.java
@@ -0,0 +1,13 @@
+package lab.malangdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalangDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalangDemoApplication.class, args);
+	}
+
+}
---
+++ malang-demo/src/test/java/lab/malangdemo/MalangDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.malangdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalangDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
