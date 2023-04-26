2d77b701ba097412a40c340588c3afc56d1117fe
Taemin Im <devTaemin@gmail.com>
2023-04-26 22:47:49 +0900

    Create Message.java

---
+++ malang-demo/src/main/java/lab/malangdemo/domain/message/dto/Message.java
@@ -0,0 +1,18 @@
+package lab.malangdemo.domain.message.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private String type;
+    private String sender;
+    private String channelId;
+    private Object data;
+}
+
